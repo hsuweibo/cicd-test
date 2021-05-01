4db707af36d72c3094fdf2af7ab1ff85a3652c28
weibo <hsuweibo@yahoo.com>
2021-05-01 02:49:07 -0400

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-             Hi there! What's up!?? How are you doing today?
+            This is changed in feature branch
         </p>
         <a
           className="App-link"
